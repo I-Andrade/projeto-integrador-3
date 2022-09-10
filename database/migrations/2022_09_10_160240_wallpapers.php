fc065155a27f016fc4bb3ce1036eb14b29fe8aa6
Tairone Amaral <taironeamaral@bol.com.br>
2022-09-10 16:43:17 -0300

    Ajustes migrations

--- database/migrations/2022_09_10_160240_wallpapers.php
+++ database/migrations/2022_09_10_160240_wallpapers.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class Wallpapers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('wallpapers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('image');
+            $table->string('category')->nullable();
+            $table->string('author');
+            $table->string('description');
+            $table->timestamps('created_at');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
     }
 }
