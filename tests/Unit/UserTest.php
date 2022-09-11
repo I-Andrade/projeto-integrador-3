800900abc98cb9e746cb2a6afc1c28bdd17004df
I.Andrade <irfley@hotmail.com>
2022-09-11 19:37:51 -0300

    Ajustes Faker e UserTest

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+/*use PHPUnit\Framework\TestCase;*/ use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function test_database_create_users()
+    {
+        $qtde = 20;
+        User::factory()->unverified()->count($qtde)->create();
+        $users = User::all();
+        $this->assertCount($qtde,$users);
+        $this->assertNotEmpty($users->first->name);
+    }
+}
