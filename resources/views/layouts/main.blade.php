fba0ac93bc3ccd5907f37b7f3cfa8d72aaa5952e
fabriciogon33 <72996535+fabriciogon33@users.noreply.github.com>
2022-09-16 09:44:22 -0300

    alteração main.blade 	modified:   resources/views/layouts/main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -36,13 +36,9 @@
 
 <body data-spy="scroll" data-target=".fixed-top">
     <!-- Preloader -->
-    <div class="spinner">
-        <div class="bounce1"></div>
-        <div class="bounce2"></div>
-        <div class="bounce3"></div>
-    </div>
 
-  {{-- @extends('layouts.preloader') --}}
+
+  @include('layouts.preloader')
     <!-- end of preloader -->
 
     <!-- Navigation -->
