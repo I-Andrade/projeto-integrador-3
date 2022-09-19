bbc08a2f0b85f39ef8038d684445b97f7e3048eb
Tairone Amaral <taironeamaral@bol.com.br>
2022-09-19 06:10:38 -0300

    Integração Swagger
    Criação dos métodos getAll e getById no swagger

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,20 +5,58 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Wallpaper;
 
+/**
+ * @OA\Swagger(
+ *   basePath="/ApiController",
+ *   @OA\Info(
+ *     title="Wallpaper API (PI3 Univesp)",
+ *     version="1.0.0"
+ *   )
+ * )
+ */
+
 class ApiController extends Controller
 {
+    /**
+    * @OA\Get(
+    *   path="/api/wallpapers",
+    *   summary="List wallpapers",
+    *   operationId="getAllWallpapers",
+    *   @OA\Response(response=200, description="successful operation"),
+    *   @OA\Response(response=406, description="not acceptable"),
+    *   @OA\Response(response=500, description="internal server error")
+    * )
+    *
+    */
     public function getAllWallpapers()
     {
         $wallpapers = Wallpaper::all();
         return response()->json($wallpapers);
     }
-    
+
+    /**
+     * @OA\Get(
+     *   path="/api/wallpaper/{Id}",
+     *   summary="List Wallpaper by ID",
+     *   operationId="getWallpaper",
+     *   @OA\Parameter(
+     *     name="Id",
+     *     in="path",
+     *     description="Target Wallpaper.",
+     *     required=true
+     *   ),
+     *   @OA\Response(response=200, description="successful operation"),
+     *   @OA\Response(response=406, description="not acceptable"),
+     *   @OA\Response(response=500, description="internal server error")
+     * )
+     *
+     */    
     public function getWallpaper($id)
     {
         $wallpaper = Wallpaper::find($id);
         return response()->json($wallpaper);
     }
-
+   
     public function createWallpaper(Request $request)
     {
         $wallpaper = Wallpaper::create($request->all());
