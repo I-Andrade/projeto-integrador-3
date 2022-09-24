6408017d133998408e876722caa36714ef3d9ec6
I.Andrade <irfley@hotmail.com>
2022-09-24 17:48:43 -0300

    Ajuste api insta test

--- resources/views/layouts/carrossel.blade.php
+++ resources/views/layouts/carrossel.blade.php
@@ -6,6 +6,10 @@
 <div class="carousel">
     <div class="wrap">
         <ul id="instafeed">
+            
+            @foreach ($fotos as $foto)
+                <img src="{{  $foto['media_url'] }}" width="200px" height="250px">
+            @endforeach
 
         </ul>
     </div>
