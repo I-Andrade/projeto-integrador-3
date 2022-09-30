7b72cb9a0414966842e6a81529d358eb45f34736
I.Andrade <irfley@hotmail.com>
2022-09-29 21:26:19 -0300

    habilita exibicao fotos instagram

--- resources/views/layouts/carrossel.blade.php
+++ resources/views/layouts/carrossel.blade.php
@@ -1,16 +1,17 @@
 <link rel="stylesheet" href="css/carrossel.css">
 
-
-{{-- <div class="carousel">
+@isset($fotos)
+<div class="carousel">  
     <div class="wrap">
         <ul id="instafeed">
             @foreach ($fotos as $foto)
-                <img src="{{  $foto['media_url'] }}" width="200px" height="250px">
+                <img src="{{  $foto->media_url }}" width="200px" height="250px">
             @endforeach
         </ul>
     </div>
-
 </div>
- --}}
+@endisset
+
+
 
 
