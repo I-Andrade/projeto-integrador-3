641f98b70a2469f85c751106268b0e6a71e6fe43
Tairone Amaral <taironeamaral@bol.com.br>
2022-10-08 16:34:47 -0300

    Ajuste para uso do HTTPS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (!\App::environment('local')) {
+            \URL::forceSchema('https');
+       }
     }
 }
