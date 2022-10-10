b071a2848f7c047f098288c7a5ce6b6e079f620e
Tairone Amaral <taironeamaral@bol.com.br>
2022-10-09 22:54:59 -0300

    Correção para testes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,8 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (!\App::environment('local')) {
-            \URL::forceSchema('https');
-       }
+        //
     }
 }
