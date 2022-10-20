db9b6dcb251667cbeaa1a57a5b9170b9d6cb453e
fabriciogon33 <72996535+fabriciogon33@users.noreply.github.com>
2022-10-19 22:40:20 -0300

    alteração pagina detalhes

--- resources/views/site/portifolio/paginadetalhe.blade.php
+++ resources/views/site/portifolio/paginadetalhe.blade.php
@@ -10,8 +10,8 @@
 
     <!-- Features -->
     <div id="features" class="filter">
-        <br> <br> <br> <br> 
-    @include('layouts.navbarPort')
+        <br> <br> <br> <br>
+        @include('layouts.navbarPort')
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
@@ -20,49 +20,81 @@
                     <h3> Lorem Ipsum</h3>
 
                 </div> <!-- end of col -->
+                <br>
 
             </div> <!-- end of row -->
             <div class="row">
 
-                <div class="basic-3">
-                    <div class="container">
 
-                        <div class="row">
+                <div class="row">
+                    <div class="col-lg-10">
+                        <br>
 
-                            <div class="col-lg-5">
-                                <div class="text-container">
+                        <div class="image-container">
+                            <img src="https://drive.google.com/uc?export=view&id=1l8sY-66kIWDTrQe9wFjHoSUXTDe-jlMG" style="width:auto; max-width: 100%; height: auto"
+                            title="Click for the larger version." />
+                        </div> <!-- end of image-container -->
+                    </div>
 
-                                    <p>Desenvolvimento de identidade visual da marca (logotipo, papelaria,
-                                        PDV), fotografia e toda parte de comunicação: criação de posts (arte),
-                                        publicações nas rede sociais, criação de campanhas sazonais com
-                                        produtos específicos para cada ação e promoções.
-                                        PÚBLICO-ALVO
-                                        Mulheres 70% | Homens 30%
-                                        Cheesecake no pote: entre 18 a 45 anos | por quilo: entre 25 a 65+ anos
-                                        Residentes na cidade de Franca/SP, com interesse em doces e sobremesas
-                                        especiais, de média a alta renda, que “não se importam” em pagar a mais
-                                        por um produto de qualidade.
-                                        TOM DE VOZ
-                                        Linguagem coloquial, descontraído e com toques de humor.
-                                        OBJETIVO
-                                        Crescer e consolidar a marca, alcançando reconhecimento no mercado de
-                                        sobremesas especiais na cidade, se tornando referência em cheesecake.
-                                        Aumentar engajamento, principalmente entre a classe alta, com o intuito
-                                        de se tornar fornecedor de grandes restaurantes</p>
+                </div>
+                <br>
 
-                                </div> <!-- end of text-container -->
-                            </div> <!-- end of col -->
-                            <br>
-                            <div class="col-lg-7">
-                                <div class="image-container">
-                                    <img class="img-fluid" src="images/details-2.png" alt="alternative">
-                                </div> <!-- end of image-container -->
-                            </div> <!-- end of col -->
-                        </div> <!-- end of row -->
-                    </div> <!-- end of container -->
-                </div> <!-- end of basic-3 -->
+                <div class="row">
 
-            </div> <!-- end of row -->
+                    <div class="col-lg-10">
+                        <div class="text-container">
+
+                            <p>Desenvolvimento de identidade visual da marca (logotipo, papelaria,
+                                PDV), fotografia e toda parte de comunicação: criação de posts (arte),
+                                publicações nas rede sociais, criação de campanhas sazonais com
+                                produtos específicos para cada ação e promoções.
+                                PÚBLICO-ALVO
+                                Mulheres 70% | Homens 30%
+                                Cheesecake no pote: entre 18 a 45 anos | por quilo: entre 25 a 65+ anos
+                                Residentes na cidade de Franca/SP, com interesse em doces e sobremesas
+                                especiais, de média a alta renda, que “não se importam” em pagar a mais
+                                por um produto de qualidade.
+                                TOM DE VOZ
+                                Linguagem coloquial, descontraído e com toques de humor.
+                                OBJETIVO
+                                Crescer e consolidar a marca, alcançando reconhecimento no mercado de
+                                sobremesas especiais na cidade, se tornando referência em cheesecake.
+                                Aumentar engajamento, principalmente entre a classe alta, com o intuito
+                                de se tornar fornecedor de grandes restaurantes</p>
+
+                        </div> <!-- end of text-container -->
+                    </div> <!-- end of col -->
+                    <br>
+
+
+                </div> <!-- end of row -->
+                <!-- end of container -->
+            </div> <!-- end of basic-3 -->
+            <div class="row">
+                <div class="col-lg-10">
+                    <br>
+
+                    <div class="image-container">
+                        <img src="https://drive.google.com/uc?export=view&id=1B_1HUM4to3b0AhuylBd3RgH7x_-4WHCy" style="width: auto; max-width: 100%; height: auto"
+                        title="Click for the larger version." />
+                        <img src="https://drive.google.com/uc?export=view&id=1sdmKiRi7UUnQw4l-l5S4tga-5jQkjTFS" style="width: auto; max-width: 100%; height: auto"
+                        title="Click for the larger version." />
+                        <br>
+                        <img src="https://drive.google.com/uc?export=view&id=1gyhhf_epuMxbOpwvqGtZzrTFMzcOxjiE" style="width: auto; max-width: 100%; height: auto"
+                        title="Click for the larger version." />
+                        <br>
+                        <img src="https://drive.google.com/uc?export=view&id=1R8_ghCIFGOS_901czGJ_IjvWuBfc-RCd" style="width: auto; max-width: 100%; height: auto"
+                        title="Click for the larger version." />
+                        <br>
+                        <img src="https://drive.google.com/uc?export=view&id=1jr7q0jXorPV8_HYAajSXahLiF6Rv3XFl" style="width: auto; max-width: 100%; height: auto"
+                        title="Click for the larger version." />
+
+                    </div> <!-- end of image-container -->
+                </div>
+
+            </div>
+
+
         </div> <!-- end of container -->
     </div> <!-- end of tabs -->
     <!-- end of features -->
