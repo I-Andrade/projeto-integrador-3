cd3ea82b360044aae6fe3959b0adf483f4ad6d31
laisriquieri <laisriquieri@gmail.com>
2022-10-25 10:47:08 -0300

    alteracao js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -95,6 +95,54 @@
 		}
     });
 
+	    /* Image Slider 2 - Swiper */
+		var imageSliderOne = new Swiper('.image-slider-1', {
+			autoplay: {
+				delay: 3000,
+				disableOnInteraction: false
+			},
+			loop: true,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			}
+		});
+	
+	
+		/* Image Slider - Swiper */
+		var imageSliderTwo = new Swiper('.image-slider-2', {
+			autoplay: {
+				delay: 2000,
+				disableOnInteraction: false
+			},
+			loop: true,
+			spaceBetween: 30,
+			slidesPerView: 5,
+			breakpoints: {
+				// when window is <= 580px
+				580: {
+					slidesPerView: 1,
+					spaceBetween: 10
+				},
+				// when window is <= 768px
+				768: {
+					slidesPerView: 2,
+					spaceBetween: 20
+				},
+				// when window is <= 992px
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 20
+				},
+				// when window is <= 1200px
+				1200: {
+					slidesPerView: 4,
+					spaceBetween: 20
+				},
+	
+			}
+		});
+
 
     /* Image Lightbox - Magnific Popup */
 	$('.popup-link').magnificPopup({
