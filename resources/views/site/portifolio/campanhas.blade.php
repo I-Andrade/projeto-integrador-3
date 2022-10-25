28d244208d3636377cabf63a840266bc410ce6e6
fabriciogon33 <72996535+fabriciogon33@users.noreply.github.com>
2022-10-24 22:36:57 -0300

    Alterações para receber os dados do back no portif

--- resources/views/site/portifolio/campanhas.blade.php
+++ resources/views/site/portifolio/campanhas.blade.php
@@ -6,7 +6,13 @@
    @section('title', 'Campanhas')
 
    @section('content')
-
+   @php
+   $campanhas = [[ 'titulo' => 'cliente 1',
+                'idImagem' => '1l8sY-66kIWDTrQe9wFjHoSUXTDe-jlMG'],
+                ['titulo' => 'cliente 2',
+                'idImagem' => '1jr7q0jXorPV8_HYAajSXahLiF6Rv3XFl']
+];
+   @endphp
 <!-- Projects -->
 <div id="projects" class="filter">
     <div class="container">
@@ -19,30 +25,12 @@
             <div class="col-lg-12">
                 @include('layouts.navbarPort')
                 <div class="grid">
+                    @foreach($campanhas as $campanha)
                     <div class="element-item embalagens">
-                        <a  href="paginadetalhe"><div class="element-item-overlay"><span>Cliente 1</span></div><img src="images\portifolio\img91.jpg" alt="alternative"></a>
+                        <a  href="paginadetalhe"><div class="element-item-overlay"><span>{{$campanha['titulo']}}</span></div><img src="https://drive.google.com/uc?export=view&id={{$campanha['idImagem']}}" alt="alternative"></a>
                     </div>
-                    <div class="element-item embalagens">
-                        <a href="#project-2"><div class="element-item-overlay"><span>Cliente 2</span></div><img src="images\portifolio\img31.jpg" alt="alternative"></a>
-                    </div>
-                    <div class="element-item identidade embalagens sites">
-                        <a href="#project-3"><div class="element-item-overlay"><span>Cliente 3</span></div><img src="images\portifolio\img105.jpg" alt="alternative"></a>
-                    </div>
-                    <div class="element-item identidade embalagens sites">
-                        <a href="#project-4"><div class="element-item-overlay"><span>Cliente 4</span></div><img src="images\portifolio\img89.jpg" alt="alternative"></a>
-                    </div>
-                    <div class="element-item identidade embalagens sites campanhas">
-                        <a href="#project-5"><div class="element-item-overlay"><span>Cliente 5</span></div><img src="images\portifolio\img80.jpg" alt="alternative"></a>
-                    </div>
-                    <div class="element-item sites campanhas identidade">
-                        <a href="#project-6"><div class="element-item-overlay"><span>Cliente 6</span></div><img src="images\portifolio\img52.jpg" alt="alternative"></a>
-                    </div>
-                    <div class="element-item identidade embalagens">
-                        <a href="#project-7"><div class="element-item-overlay"><span>Cliente 7</span></div><img src="images\portifolio\img97.jpg" alt="alternative"></a>
-                    </div>
-                    <div class="element-item identidade embalagens">
-                        <a href="#project-8"><div class="element-item-overlay"><span>Cliente 8</span></div><img src="images\portifolio\img54.jpg" alt="alternative"></a>
-                    </div>
+                    @endforeach
+
                 </div> <!-- end of grid -->
                 <!-- end of filter -->
 
--- resources/views/site/portifolio/paginadetalhe.blade.php
+++ resources/views/site/portifolio/paginadetalhe.blade.php
@@ -7,100 +7,94 @@
 @section('title', 'Fernanda Carrijo')
 
 @section('content')
+    @php
+        $detalhes = [
+            [
+                'titulo' => 'Café Terreiro',
+                'idImagemCapa' => '1l8sY-66kIWDTrQe9wFjHoSUXTDe-jlMG',
+                'categoria' => 'Identidade Visual',
+                'descricao' => 'Desenvolvimento de identidade visual da marca (logotipo, papelaria, PDV), fotografia e
+                 toda parte de comunicação: criação de posts (arte), publicações nas rede sociais, criação de campanhas
+                  sazonais com produtos específicos para cada ação e promoções. PÚBLICO-ALVO Mulheres 70% | Homens 30%
+                   Cheesecake no pote: entre 18 a 45 anos | por quilo: entre 25 a 65+ anos Residentes na cidade de Franca/SP,
+                    com interesse em doces e sobremesas especiais, de média a alta renda, que “não se importam” em pagar
+                     a mais por um produto de qualidade. TOM DE VOZ Linguagem coloquial, descontraído e com toques de humor.
+                      OBJETIVO Crescer e consolidar a marca, alcançando reconhecimento no mercado de sobremesas especiais na
+                       cidade, se tornando referência em cheesecake. Aumentar engajamento, principalmente entre a classe alta
+                       , com o intuito de se tornar fornecedor de grandes restaurantes',
+            ],
+        ];
+        $IdImagens = ['1B_1HUM4to3b0AhuylBd3RgH7x_-4WHCy', '1sdmKiRi7UUnQw4l-l5S4tga-5jQkjTFS', '1gyhhf_epuMxbOpwvqGtZzrTFMzcOxjiE', '1R8_ghCIFGOS_901czGJ_IjvWuBfc-RCd', '1jr7q0jXorPV8_HYAajSXahLiF6Rv3XFl'];
+
+    @endphp
 
     <!-- Features -->
     <div id="features" class="filter">
         <br> <br> <br> <br>
         @include('layouts.navbarPort')
         <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h2>GREKO
-                        CHEESECAKE</h2>
-                    <h3> Lorem Ipsum</h3>
-
-                </div> <!-- end of col -->
-                <br>
-
-            </div> <!-- end of row -->
-            <div class="row">
-                <div class="image-container">
-                    <img src="https://drive.google.com/file/d/1R8_ghCIFGOS_901czGJ_IjvWuBfc-RCd/view" style="width:auto; max-width: 100%; height: auto"
-                    title="Click for the larger version." />
-                </div> <
-
-
+            @foreach ($detalhes as $detalhe)
                 <div class="row">
-                    <div class="col-lg-10">
+                    <div class="col-lg-12">
+                        <h2>{{ $detalhe['titulo'] }}</h2>
 
-                        <br>
 
-                        <div class="image-container">
-                            <img src="https://drive.google.com/uc?export=view&id=1l8sY-66kIWDTrQe9wFjHoSUXTDe-jlMG" style="width:auto; max-width: 100%; height: auto"
-                            title="Click for the larger version." />
-                        </div> <!-- end of image-container -->
-                    </div>
-
-                </div>
-                <br>
-
-                <div class="row">
-
-                    <div class="col-lg-10">
-                        <div class="text-container">
-
-                            <p>Desenvolvimento de identidade visual da marca (logotipo, papelaria,
-                                PDV), fotografia e toda parte de comunicação: criação de posts (arte),
-                                publicações nas rede sociais, criação de campanhas sazonais com
-                                produtos específicos para cada ação e promoções.
-                                PÚBLICO-ALVO
-                                Mulheres 70% | Homens 30%
-                                Cheesecake no pote: entre 18 a 45 anos | por quilo: entre 25 a 65+ anos
-                                Residentes na cidade de Franca/SP, com interesse em doces e sobremesas
-                                especiais, de média a alta renda, que “não se importam” em pagar a mais
-                                por um produto de qualidade.
-                                TOM DE VOZ
-                                Linguagem coloquial, descontraído e com toques de humor.
-                                OBJETIVO
-                                Crescer e consolidar a marca, alcançando reconhecimento no mercado de
-                                sobremesas especiais na cidade, se tornando referência em cheesecake.
-                                Aumentar engajamento, principalmente entre a classe alta, com o intuito
-                                de se tornar fornecedor de grandes restaurantes</p>
-
-                        </div> <!-- end of text-container -->
                     </div> <!-- end of col -->
                     <br>
 
-
                 </div> <!-- end of row -->
-                <!-- end of container -->
-            </div> <!-- end of basic-3 -->
-            <div class="row">
-                <div class="col-lg-10">
+                <div class="row">
+
+
+
+                    <div class="row">
+                        <div class="col-lg-10">
+
+                            <br>
+
+                            <div class="image-container">
+                                <img src="https://drive.google.com/uc?export=view&id={{ $detalhe['idImagemCapa'] }}"
+                                    style="width:auto; max-width: 100%; height: auto"
+                                    title="Click for the larger version." />
+                            </div> <!-- end of image-container -->
+                        </div>
+
+                    </div>
                     <br>
 
-                    <div class="image-container">
-                        <img src="https://drive.google.com/uc?export=view&id=1B_1HUM4to3b0AhuylBd3RgH7x_-4WHCy" style="width: auto; max-width: 100%; height: auto"
-                        title="Click for the larger version." />
-                        <img src="https://drive.google.com/uc?export=view&id=1sdmKiRi7UUnQw4l-l5S4tga-5jQkjTFS" style="width: auto; max-width: 100%; height: auto"
-                        title="Click for the larger version." />
+                    <div class="row">
+
+                        <div class="col-lg-10">
+                            <div class="text-container">
+
+                                <p>{{ $detalhe['descricao'] }}</p>
+
+                            </div> <!-- end of text-container -->
+                        </div> <!-- end of col -->
                         <br>
-                        <img src="https://drive.google.com/uc?export=view&id=1gyhhf_epuMxbOpwvqGtZzrTFMzcOxjiE" style="width: auto; max-width: 100%; height: auto"
-                        title="Click for the larger version." />
-                        <br>
-                        <img src="https://drive.google.com/uc?export=view&id=1R8_ghCIFGOS_901czGJ_IjvWuBfc-RCd" style="width: auto; max-width: 100%; height: auto"
-                        title="Click for the larger version." />
-                        <br>
-                        <img src="https://drive.google.com/uc?export=view&id=1jr7q0jXorPV8_HYAajSXahLiF6Rv3XFl" style="width: auto; max-width: 100%; height: auto"
-                        title="Click for the larger version." />
-
-                    </div> <!-- end of image-container -->
-                </div>
-
-            </div>
+            @endforeach
 
 
-        </div> <!-- end of container -->
+        </div> <!-- end of row -->
+        <!-- end of container -->
+    </div> <!-- end of basic-3 -->
+    <div class="row">
+        <div class="col-lg-10">
+            <br>
+
+            <div class="image-container">
+                @foreach ($IdImagens as $id)
+                    <img src="https://drive.google.com/uc?export=view&id={{ $id}}"
+                        style="width: auto; max-width: 100%; height: auto" title="Click for the larger version." />
+                @endforeach
+
+            </div> <!-- end of image-container -->
+        </div>
+
+    </div>
+
+
+    </div> <!-- end of container -->
     </div> <!-- end of tabs -->
     <!-- end of features -->
 
