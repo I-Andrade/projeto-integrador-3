36a38f2112e457e898dab92132896a5e8e3cab88
I.Andrade <irfley@hotmail.com>
2022-11-16 07:51:36 -0300

    Ajustes Login

--- resources/views/site/admin/login.blade.php
+++ resources/views/site/admin/login.blade.php
@@ -1,28 +1,22 @@
 @extends('layouts.mainAdmin')
 
-@section('title', 'Login')
+@section('title', 'Fernanda?')
 
 @section('content')
     <link rel="stylesheet" href="css/blog.css">
 
     <div id="contact" class="form-2">
 
-        <div class="container">
+        <div class="container-login">
 
             <div class="row">
-                <div class="loginBox">
-                    <div class="loginBox">
-
-                        <h2>Login</h2>
-
-
-
-
-                        <img class="decorative-line" src="images/decorative-line.svg" alt="decorative line">
+                <div class="col-lg-12-login">
+                    <div style="margin-left: 1.75rem;">
+                        <h2><b style="color:#E45032">Fernanda</b>... você por aqui?</h2>
                     </div>
 
 
-                    <div class="col">
+                    <div class="col-lg-12-login">
                         <form class="form-group" method="POST" action="{{ route('login') }}">
                             @csrf
 
@@ -30,33 +24,33 @@
                             <!-- Email Address -->
                             <div class="form-floating">
                                 <input id="email" class="form-control" type="email" name="email"
-                                    value="{{ old('email') }}" required autofocus placeholder="nome@examplo.com.br">
-                                <label for="floatingInput">E-mail</label>
+                                    value="{{ old('email') }}" required autofocus placeholder="seu-email@aqui.com.br">
+                                <label for="floatingInput"><!-- Email Address --></label>
                             </div>
 
                             <!-- Password -->
                             <div class="form-floating">
                                 <input id="password" class="form-control" type="password" name="password"
                                     placeholder="senha" required>
-                                <label for="floatingPassword">Senha</label>
+                                <label for="floatingPassword"><!-- Password --></label>
                             </div>
 
                             <!-- Remember Me -->
-                            <div class="checkbox mb-3">
+                            {{-- <div class="checkbox mb-3">
                                 <label for="remember_me">
                                     <input id="remember_me" type="checkbox" value="remember-me" name="remember"> Manter
                                     logado
                                 </label>
-                            </div>
+                            </div> --}}
 
-                            <div class="form-floating">
+                            {{-- <div class="form-floating">
                                 @if (Route::has('password.request'))
                                     <a class="underline text-sm text-gray-600 hover:text-gray-900"
                                         href="{{ route('password.request') }}">
                                         {{ __('Forgot your password?') }}
                                     </a>
                                 @endif
-                            </div>
+                            </div> --}}
 
                             <div class="form-group">
                                 <button type="submit" class="form-control-submit-button">Avançar</button>
