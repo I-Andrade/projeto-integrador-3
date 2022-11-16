72a33b6ecc1d2334da95d3a80c3c79b475bf294f
I.Andrade <irfley@hotmail.com>
2022-11-16 07:51:52 -0300

    Ajustes Home

--- resources/views/layouts/carrossel.blade.php
+++ resources/views/layouts/carrossel.blade.php
@@ -1,42 +1,17 @@
 <link rel="stylesheet" href="css/carrossel.css">
 
 @isset($fotos)
- <div class="outer-container">{{-- carrossel de imagens do instagram --}}
+ <div class="outer-container" style="margin-top: 50px; max-height: 400px;">
         <div class="slider-container">
-        <div class="swiper-container image-slider-2">
-            <div class="swiper-wrapper">
-                <input type="radio" name="radio-btn" id="radio1">
-                <input type="radio" name="radio-btn" id="radio2">
-                <input type="radio" name="radio-btn" id="radio3">
-                <input type="radio" name="radio-btn" id="radio4">
-                <div class="slide-first" >
-                </div>
-                        @foreach ($fotos as $foto)
-                        <div class="swiper-slide">
-                            <img class="img" src="{{ $foto->media_url }}" >
+            <div class="swiper-container image-slider-2" style="max-height: 400px;">
+                <div class="swiper-wrapper">                  
+                    @foreach ($fotos as $foto)
+                        <div class="swiper-slide" style="max-height: 400px;">
+                            <img class="img" style="min-width: 100%; max-width: 300px;" src="{{ $foto->media_url }}" >
                         </div>
-
-                        @endforeach
-
-
-                <div class="navigation-auto">
-                    <div class="auto-btn1"></div>
-                    <div class="auto-btn2"></div>
-                    <div class="auto-btn3"></div>
-                    <div class="auto-btn4"></div>
-                </div>
-                <div class="manual-navigation">
-                    <label for="radio1" class="manual-btn"></label>
-                    <label for="radio2" class="manual-btn"></label>
-                    <label for="radio3" class="manual-btn"></label>
-                    <label for="radio4" class="manual-btn"></label>
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
-
-</div>
-
-<script src="\js\carrossel.js"></script>
-
 @endisset
